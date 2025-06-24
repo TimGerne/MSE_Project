4afea13aa917ffec2a1426dce4b9659a18f44aa8
Tim <gernetim@gmail.com>
2025-06-24 15:54:39 +0200

    First draft of crawler - does not process crawled websites yer

---
+++ crawler.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urlparse, urljoin
+
+seeds = ['https://www.wikipedia.org']
+visited = set()
+parsers = {}
+
+
+def parsing_allowed(url):
+    # get domain
+    domain = urlparse(url).netloc
+
+    # check if parser exists and use or create it
+    if domain in parsers:
+        rp = parsers[domain]
+    else:
+        rp = RobotFileParser()
+        rp.set_url(f"https://{domain}/robots.txt")
+        parsers[domain] = rp
+
+    # check if robots.tsx does exist and is readable
+    try:
+        rp.read()
+    # if not allow to crawl
+    except:
+        return True
+
+    # true if parsing allowed or not specified for our parser
+    return rp.can_fetch("MyCrawler 1.0", url)
+
+
+def get_crawl_delay(url, default_delay=1):
+    # TODO make this more efficient as e.g. combine with parsing_allowed()
+    domain = urlparse(url).netloc
+    # domain should be in parser at this point as we called parsing_allowed before
+
+    if domain in parsers:
+        rp = parsers[domain]
+    else:
+        print(parsers)
+        raise ('TEST')
+
+    delay = rp.crawl_delay("MyCrawler 1.0")
+    if delay:
+        return delay
+    else:
+        return default_delay
+
+
+# crawls the given url recursively
+def crawl(url):
+    if url in visited:
+        print('URL has already been visited')
+        return
+
+    visited.add(url)
+
+    # check robots.tsx
+    if not parsing_allowed(url):
+        print('URL not allowed')
+
+        return
+
+    response = requests.get(url, headers={'User-Agent': 'MyCrawler 1.0'})
+
+    # check if website is available
+    if response.status_code != 200:
+        print('URL returns wrong code')
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # do something with the content
+    try:
+        print(soup.find('title').text)
+        print(url)
+        print()
+    except:
+        print(f'Page has no title')
+        print(url)
+        print()
+
+    for link in soup.find_all('a', href=True):
+
+        # just returns lik['href'] is the link is absolute
+        next_url = urljoin(url, link['href'])
+
+        if next_url.startswith('http'):
+            time.sleep(get_crawl_delay(url))
+            if next_url not in visited:
+                crawl(next_url)
+
+
+def main():
+    crawl(seeds[0])
+    print(f'Number of visited sites: {len(visited)}')
+
+
+if __name__ == '__main__':
+    main()
