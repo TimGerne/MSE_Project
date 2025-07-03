0ad24eb3be386705c8b077618f5f4969ea8ca12f
moemchen13 <super.moritz.christ@gmx.de>
2025-07-03 21:03:34 +0200

    next fix is showing of results

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import io
 
 #Examples inputs################
 example_retrieved_docs = pd.DataFrame({"Query":[0,0,0,0,1,1,1,1],
@@ -10,17 +11,6 @@ example_retrieved_docs = pd.DataFrame({"Query":[0,0,0,0,1,1,1,1],
                                        "Relevance":[1,0.5,0.3,0.1,1,0.5,0.3,0.1]})
 example_queries = ["Tübingen","Food"]
 #################################
-
-
-def start_crawl():
-    st.session_state.crawling = True
-    st.session_state.has_crawled = True
-    #TODO:add more function that actually starts crawler.py
-
-def stop_crawl():
-    st.session_state.crawling = False
-    #TODO add pause function for crawler.py
-
 
 def retrieve_queries(query:str,query_file)->list:
     queries = []
@@ -33,89 +23,86 @@ def retrieve_queries(query:str,query_file)->list:
     return queries
     
 
-def start_searching(query,queries):
-    retrieve_queries(query,queries)
-    with st.spinner(text="Search in Progress"):
-        time.sleep(2)
-        #TODO: Replace by actual start search call
-        st.success("Retrieved Documents")
+def start_searching(query,queries)->list:
+    if queries or query:
+        queries = retrieve_queries(query,queries)
+        st.toast("Started Search")
+        with st.spinner(text="Search in Progress"):
+            time.sleep(2)
+            #docs = start_search(queries)
+            docs=None
+            #TODO: Replace by actual start search call
+            st.success("✅ Retrieved Documents")
+            return docs
+    else:
+        st.error("❌ Search failed: no query")
+        return None
 
 
-st.set_page_config(layout="wide")
+def get_queries(query:str|None,queries_file:st.runtime.uploaded_file_manager.UploadedFile)->list:
+    if queries_file:
+        stringio = io.StringIO(queries_file.getvalue().decode("utf-8"))
+        queries = stringio.read()
+        queries = queries.replace("\r","\n")
+        queries = queries.split("\n")
+        return queries
+    elif query:
+        return [query]
+    else:
+        return None
+
+
+st.set_page_config(page_title="Search",page_icon="🔎",layout="wide")
+
+#st.sidebar.success("Select a demo above.")
 st.title("Our search engine")
 queries= ["one","two"]
 
-if "has_crawled" not in st.session_state:
-    st.session_state.has_crawled=False
-if "crawling" not in st.session_state:
-    st.session_state.crawling=False
 if "has_searched" not in st.session_state:
     st.session_state.has_searched=False
 if "ndocs" not in st.session_state:
-    st.session_state.ndocs=0
+    st.session_state.ndocs=10000 #Look for documents
 
+st.markdown(f':orange-badge[{st.session_state.ndocs} documents to search]')
 
-left_column, right_column = st.columns(2)
+queries_file=None
+query=None
 
-with left_column:
-
-    st.subheader("Crawling")
-
-    button_side,time_side = st.columns([5,2])
-    with button_side:    
-        if st.session_state.crawling:
-            st.button('Pause Crawling',on_click=stop_crawl)
-        elif st.session_state.has_crawled:
-            st.button('Resume Crawling',on_click=start_crawl)
-        else:
-            st.button('Start Crawling',on_click=start_crawl)
-    with time_side:
-        d=0
-
-    #on-click activation
-    latest_iteration = st.empty()
-    crawl_bar = st.progress(0)
-    for i in range(100):
-        # Update the progress bar with each iteration.
-        latest_iteration.text(f'Percentage of Crawl {i+1}%')
-        crawl_bar.progress(i + 1)
-        time.sleep(0.01)
-
-    st.markdown(f':green-badge[Crawling completed] :orange-badge[{st.session_state.ndocs} documents]')
+with st.form("search_form"):
+    queries_file = st.file_uploader("Upload Queries via file 📁",help="Upload tab separated format",accept_multiple_files=False) 
+    query = st.text_input("or put your Query here 👇",placeholder="What are you looking for?",help="Accept only one lined query")
     
-    
-    st.subheader("Search")
+#Search
 
-    with st.form("search_form"):
-        queries_file = st.file_uploader("Upload Queries via file 📁",help="Upload tab separated format") 
-        query = st.text_input("or put your Query here 👇",placeholder="What are you looking for?",help="Accept only one lined query")
-            
-    #Search    
-        submitted = st.form_submit_button('Start Search 🔎',disabled=st.session_state.has_crawled,on_click=start_searching)
-        if submitted:
-            start_searching(query,queries_file)
-
-
-
+    submitted = st.form_submit_button('Start Search 🔎')#,on_click=start_searching)
+    if submitted:
+        queries = get_queries(query,queries_file)
+        docs = start_searching(query,queries_file)
+        
 
 
 retrieved_documents=None
 
-with right_column:
+queries = example_queries#
 
-    
-    queries = example_queries#
-    
-    st.subheader("Search results")
+left,right = st.columns([6,1])
+with left:
+    st.markdown("### Search results")
+with right:
+    n_results_to_show = st.number_input("Number of results",min_value=1,value=3,label_visibility="visible")
 
-    query_tabs_name = [f"Results {i}" for i in range(len(queries))]
-    query_tabs = st.tabs(query_tabs_name)
-    for i,(query,tab) in enumerate(zip(queries,query_tabs)):
-        tab.write(f"Query: {query}")
+query_tabs_name = [f"Results {i}" for i in range(len(queries))]
+query_tabs = st.tabs(query_tabs_name)
+for i,(query,tab) in enumerate(zip(queries,query_tabs)):
+    with tab:
+        ls,rs = st.columns([7,1])
+        ls.markdown(f"##### Query: {query}")
+        rs.download_button("Download",example_retrieved_docs[example_retrieved_docs["Query"]==i].to_csv(index=False,header=False,sep='\t'),file_name=f"results_query_{query}.tsc")
         retrieved_docs = example_retrieved_docs.loc[example_retrieved_docs["Query"]==i].iloc[:,1:]
+        shown_docs = retrieved_docs.iloc[:n_results_to_show,:]
         #tab.dataframe(retrieved_docs)
         tab.dataframe(
-            retrieved_docs,
+            shown_docs,
             column_config={
                 "Rank": st.column_config.NumberColumn("Rank",width="small"),
                 "URL": st.column_config.LinkColumn("URL",width="mid"),
@@ -124,7 +111,10 @@ with right_column:
             hide_index=True,
         )
 
-    st.download_button('download search results', example_retrieved_docs.to_csv(index=False,header=False,sep='\t'),file_name="Results.tsv")
+st.download_button('Download all results', example_retrieved_docs.to_csv(index=False,header=False,sep='\t'),file_name="Results.tsv")
 
 #@st.cache_resource for ML resources
 #https://docs.streamlit.io/get-started/fundamentals/advanced-concepts #DB connection
+#st.html("<p>Foo bar <p>")
+#st.metric("Some Value",42,2)
+#st.table() might be helpful for results.
