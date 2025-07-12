37d8c00c54dda51321bd652b8af9d09105c9c4d5
Tim <gernetim@gmail.com>
2025-07-12 12:23:44 +0200

    added function for hashes

--- crawler_file_IO.py
+++ crawler_file_IO.py
@@ -16,7 +16,7 @@ def save_frontier(filename: str, frontier: list) -> None:
             writer.writerow(entry)
 
 
-def save_visited(filename: str, visited: set) -> None:
+def save_set_to_csv(filename: str, visited: set) -> None:
     with open(filename, 'w') as f:
         writer = csv.writer(f)
         for url in visited:
@@ -29,7 +29,7 @@ def empty_file(filename: str) -> None:
     if os.path.exists(filepath):
         # as we do not open in append mode we empty the file by writing nothing in it
         open(filename, 'w').close()
-    print('Emptying done')
+    print(f'Emptied {filename}')
 
 
 def read_saved_frontier(filename: str) -> list:
@@ -49,4 +49,13 @@ def read_saved_visited(filename: str) -> list:
         reader = csv.reader(csvfile)
         for row in reader:
             result.extend(row)
+    return result
+
+
+def read_saved_hashes(filename: str) -> list:
+    result = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            result.append(int(row[0]))
     return result
